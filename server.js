e55dc98dc6ce7b4cfed240d7f5b2a99f0dfe323f
David Hoang <davidhoang455@gmail.com>
2025-05-28 17:42:06 +0000

    Add user folder and json file

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.set('view engine', 'ejs');
 //this will allow us to serve up static files, CSS, images & JS
 app.use(express.static(__dirname));
 
+//reference test json file of users
+var data = require('./test.json');
+
 //index/home URL
 app.get('/',(req,res)=>{
   let title = "Home Page";
@@ -24,6 +27,13 @@ app.get('/',(req,res)=>{
 app.get('/about',(req,res)=>{
   let title = "About Page";
   res.render('pages/about', {'title': title});
+
+});
+
+//Users route
+app.get('/users',(req,res)=>{
+  let title = "Users Page";
+  res.render('users/index', {'title': title});
 
 });
 
@@ -41,5 +51,6 @@ app.get('/gaming', (req, res) => {
 //Set server to listen for requests
 app.listen(port, () => {
   console.log(`Server running at port: ${port}`);
+  console.log(data);
 });
 
